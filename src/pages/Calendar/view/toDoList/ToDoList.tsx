cee38dde2dedd3b8093144ba91263c8594baa18d
Anton N <at0xa32@gmail.com>
2025-03-19 20:44:28 +0300

    fix

--- src/pages/Calendar/view/toDoList/ToDoList.tsx
+++ src/pages/Calendar/view/toDoList/ToDoList.tsx
@@ -43,6 +43,7 @@ const ToDoList = ({ list }: IToDoList) => {
   return (
     <>
     {actionProcessing ? <Loading /> : ''}
+    
       {!isMobile && (
         <div className={b()}>
           <div className={b('header')}>
