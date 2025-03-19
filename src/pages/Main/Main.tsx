319f57b4d5556ea22ecf904c539d4f71f86f4d97
Anton N <at0xa32@gmail.com>
2025-03-19 21:20:37 +0300

    news

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,18 +1,20 @@
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 import block from 'bem-cn';
 import styles from './Main.module.scss';
-import { getNotify } from '@/components/Notify/Notify';
-import Button from '@/components/Button/Buttons';
 
 const b = block(styles.main);
 
 const Main = memo(() => {
   return (
     <div className={b()}>
+      <div style={{ fontSize: '24px' }}>Новости</div>
+      <hr style={{ width: '100%' }} />
       <div className={b('news')}>
-        <div>Обновление :</div>
-        <div>✅ 1 - Были пофишкены все подсвеченые баги </div>
-        <div>✅ 2 - Добавлен функционал для фильтрации сообщений </div>
+        <div className={b('item')}>
+          <div>Обновление 19.03.2025</div>
+          <div>✅ 1 - Были пофишкены все подсвеченые баги </div>
+          <div>✅ 2 - Добавлен функционал для фильтрации сообщений </div>
+        </div>
       </div>
     </div>
   );
